8770573fa354775d100408d564cb7d8db0889f32
Chavez Harris <codedbychavez@gmail>
2022-11-19 17:15:05 -0300

    cleanup

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function closeMobileMenu() {
-  var elem = document.getElementById('mobile-menu')
-  elem.classList.add('hide')
-}
-
-function openMobileMenu() {
-  var elem = document.getElementById('mobile-menu')
-  elem.classList.remove('hide')
-}
